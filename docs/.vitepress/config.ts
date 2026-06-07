fb2c650a40bf977490f357c7710800ff1628bbfb
qsyi <atooimitation@gmail.com>
2026-06-08 07:58:34 +0900

    og image update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,6 +3,14 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   appearance: 'force-light',
   title: 'qsToolBox',
+  head: [
+    ['meta', { property: 'og:title', content: 'qsToolBox' }],
+    ['meta', { property: 'og:description', content: 'アバター改変効率化ツール' }],
+    ['meta', { property: 'og:image', content: 'https://qsyi.github.io/qsToolbox/og-image.png' }],
+    ['meta', { property: 'og:url', content: 'https://qsyi.github.io/qsToolbox/' }],
+    ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+    ['meta', { name: 'twitter:image', content: 'https://qsyi.github.io/qsToolbox/og-image.png' }],
+  ],
   description: 'アバター改変効率化ツール',
   lang: 'ja',
   base: '/qsToolbox/',
